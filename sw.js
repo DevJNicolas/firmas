427465dfcce68d564ad91028b77e06cc9cec27bb
DevJNicolas <126189786+DevJNicolas@users.noreply.github.com>
2024-02-21 09:43:57 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "v1_cache_firmas",
+  urlsToCache = [
+    "https://script.google.com/a/macros/servinformacion.com/s/AKfycbxkBF20IzEw7LgtnQfIOH0w0DWFrhyKpwbZA3vHwIs94KbrTSOQZpwGqbyzxiL6Wne4/exec",
+    "./manifest.json",
+  ];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => "fallo el registro del cache ", err)
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const caheWitheList = [CACHE_NAME];
+
+  e.waitUntil(
+    caches.keys().then((cachesNames) =>
+      cachesNames.map((cacheName) => {
+        if (cacheName.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
